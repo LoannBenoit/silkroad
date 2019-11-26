8da5d1c86377c265513f61af353625e08a119471
SullivanMICHEL <sullivan.michel@icloud.com>
2019-11-26 10:50:59 +0100

    create folder images

---
+++ js/catalog1.js
@@ -0,0 +1,12 @@
+var catalog = [{
+	"name": "PC10 Bazooka",
+	"description": "Intel Core i5-9400F (2.9 GHz) 16 Go SSD 480 Go NVIDIA GeForce RTX 2060 6 Go Wi-FI N Windows 10 Famille 64 bits (monté)",
+	"image": "images/PC10_Bazooka.jpg",
+	"price": "1079.95"
+},
+{
+	"name": "PC CézaN",
+	"description": "AMD Ryzen 7 3700X (3.6 GHz) 16 Go SSD NVMe 480 Go + HDD 2 To AMD Radeon RX 5700 XT 8Go (sans OS - monté)",
+	"image": "images/PC_Cezan.jpg",
+	"price": "1549.95"
+}]
