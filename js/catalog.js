3706a6347f48f563c8c17d4b67a1b2b6f507b3ba
arnaudGonin <arnaud.gonin@live.fr>
2019-12-17 14:23:39 +0100

    ajout du catalogue de vaisseau spatiaux

---
+++ js/catalog.js
@@ -0,0 +1,56 @@
+[
+    {
+        "name": "Banu souli- Merchantman",
+        "description": "Heavy Freight",
+        "image": "https://vignette.wikia.nocookie.net/cheaseeating-citizens/images/7/76/Merchantman.jpg/revision/latest?cb=20140807151234&path-prefix=fr",
+        "price": "500€"
+    },
+    {
+        "name": "Banu souli - Defender",
+        "description": "light fighter",
+        "image": "https://image.playerauctions.com/Uploads/MA/MadFable/20190924105717123.jpg",
+        "price": "100€"
+    },
+    {
+        "name": "Drake Interplanetary - Cutlass Black",
+        "description": "Medium Fighter / Medium Freight",
+        "image": "https://i.imgur.com/wFG8NRr.jpg",
+        "price": "120€"
+    },
+    {
+        "name": "RSI - Aurora MR",
+        "description": "light Freight",
+        "image": "https://robertsspaceindustries.com/media/uzi8ifmut9dw3r/store_slideshow_small/Aurora-MR.jpg",
+        "price": "30€"
+    },
+    {
+        "name": "Consolidated Outland",
+        "description": "light Freight",
+        "image": "https://robertsspaceindustries.com/media/uzi8ifmut9dw3r/store_slideshow_small/Aurora-MR.jpg",
+        "price": "35€"
+    },
+    {
+        "name": "Consolidated Outland",
+        "description": "light Freight",
+        "image": "https://robertsspaceindustries.com/media/uzi8ifmut9dw3r/store_slideshow_small/Aurora-MR.jpg",
+        "price": "35€"
+    },
+    {
+        "name": "Aegis Dynamics - Avenger",
+        "description": "light freight",
+        "image": "https://robertsspaceindustries.com/media/3dx8jqsd79dmpr/store_slideshow_small/Avenger_storefront_visualjpg.jpg",
+        "price": "72€"
+    },
+    {
+        "name": "RSI - Constellation aquila",
+        "description": "Exploration",
+        "image": "https://robertsspaceindustries.com/media/4bf10yrayk2ejr/store_slideshow_small/Aquila_InFlight-Min-1.jpg",
+        "price": "340€"
+    },
+    {
+        "name": "Anvil - Carrack",
+        "description": "Exploration",
+        "image": "https://media.robertsspaceindustries.com/g7dx300udpe1v/store_small.jpg",
+        "price": "550€"
+    },
+]
