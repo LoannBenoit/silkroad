0fad672d15ea3e8a7212604291ff049b7a7371fc
Ethan <ethan.caillot@gmail.com>
2020-01-14 16:05:13 +0100

    modif catalog4

--- js/catalog4.js
+++ js/catalog4.js
@@ -4,31 +4,31 @@ var catalogue = [
   		"description" : "Une pomme bien juteuse",
 	    "image" : "Images/Pomme.jpg",
 		"price" : "0.4"
-	}
+	},
 	{
 		"name" : "Banane", 
 		"description" : "Une banane", 
 		"image" : "Images/Banane.jpg", 
 		"prince" : "0.5"
-	}
+	},
 	{
 		"name" : "Orange", 
 		"description" : "Une orange à consommer sans modération", 
 		"image" : "Images/Orange.jpg", 
 		"prince" : "0.3"
-	}
+	},
 	{
 		"name" : "Kiwi", 
 		"description" : "Un kiwi de nouvelle Zelande", 
 		"image" : "Images/Kiwi.jpg", 
 		"prince" : "0.7"
-	}
+	},
 	{
 		"name" : "Salade", 
 		"description" : "Une salade bien fraîche", 
 		"image" : "Images/Salade.jpg", 
 		"prince" : "1"
-	}
+	},
 	{
 		"name" : "Fraise", 
 		"description" : "Une seule fraise unique", 
