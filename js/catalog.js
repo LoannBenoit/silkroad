21ac9ad1e9c0e31d9fd198d63b59c86039a4f641
arnaudGonin <arnaud.gonin@live.fr>
2020-01-21 15:02:29 +0100

    refact-(Ship picture, catalog change, picture added in images/Vaisseaux)

--- js/catalog.js
+++ js/catalog.js
@@ -2,55 +2,49 @@
     {
         "name": "Banu souli- Merchantman",
         "description": "Heavy Freight",
-        "image": "https://vignette.wikia.nocookie.net/cheaseeating-citizens/images/7/76/Merchantman.jpg/revision/latest?cb=20140807151234&path-prefix=fr",
+        "image": "../images/vaisseaux/Merchantman",
         "price": "500€"
     },
     {
         "name": "Banu souli - Defender",
         "description": "light fighter",
-        "image": "https://image.playerauctions.com/Uploads/MA/MadFable/20190924105717123.jpg",
+        "image": "../images/vaisseaux/Defender",
         "price": "100€"
     },
     {
         "name": "Drake Interplanetary - Cutlass Black",
         "description": "Medium Fighter / Medium Freight",
-        "image": "https://i.imgur.com/wFG8NRr.jpg",
+        "image": "../images/vaisseaux/Cutlass",
         "price": "120€"
     },
     {
         "name": "RSI - Aurora MR",
         "description": "light Freight",
-        "image": "https://robertsspaceindustries.com/media/uzi8ifmut9dw3r/store_slideshow_small/Aurora-MR.jpg",
+        "image": "../images/vaisseaux/Aurora",
         "price": "30€"
     },
     {
-        "name": "Consolidated Outland",
+        "name": "Consolidated Outland - Mustang Alpha",
         "description": "light Freight",
-        "image": "https://robertsspaceindustries.com/media/uzi8ifmut9dw3r/store_slideshow_small/Aurora-MR.jpg",
-        "price": "35€"
-    },
-    {
-        "name": "Consolidated Outland",
-        "description": "light Freight",
-        "image": "https://robertsspaceindustries.com/media/uzi8ifmut9dw3r/store_slideshow_small/Aurora-MR.jpg",
+        "image": "../images/vaisseaux/Mustang",
         "price": "35€"
     },
     {
         "name": "Aegis Dynamics - Avenger",
         "description": "light freight",
-        "image": "https://robertsspaceindustries.com/media/3dx8jqsd79dmpr/store_slideshow_small/Avenger_storefront_visualjpg.jpg",
+        "image": "../images/vaisseaux/Avenger",
         "price": "72€"
     },
     {
         "name": "RSI - Constellation aquila",
         "description": "Exploration",
-        "image": "https://robertsspaceindustries.com/media/4bf10yrayk2ejr/store_slideshow_small/Aquila_InFlight-Min-1.jpg",
+        "image": "../images/vaisseaux/Constellation",
         "price": "340€"
     },
     {
         "name": "Anvil - Carrack",
         "description": "Exploration",
-        "image": "https://media.robertsspaceindustries.com/g7dx300udpe1v/store_small.jpg",
+        "image": "../images/vaisseaux/Carrack",
         "price": "550€"
     },
 ]
