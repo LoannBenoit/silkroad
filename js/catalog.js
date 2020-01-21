68d5e76f7efd830dd422f76d1b1351c49459f31c
arnaudGonin <arnaud.gonin@live.fr>
2020-01-21 15:28:44 +0100

    refac-(ship added in catalog)

--- js/catalog.js
+++ js/catalog.js
@@ -2,49 +2,67 @@
     {
         "name": "Banu souli- Merchantman",
         "description": "Heavy Freight",
-        "image": "../images/vaisseaux/Merchantman",
+        "image": "../images/vaisseaux/Merchantman.jpg",
         "price": "500€"
     },
     {
         "name": "Banu souli - Defender",
         "description": "light fighter",
-        "image": "../images/vaisseaux/Defender",
+        "image": "../images/vaisseaux/Defender.jpg",
         "price": "100€"
     },
     {
         "name": "Drake Interplanetary - Cutlass Black",
         "description": "Medium Fighter / Medium Freight",
-        "image": "../images/vaisseaux/Cutlass",
+        "image": "../images/vaisseaux/Cutlass.jpg",
         "price": "120€"
     },
     {
         "name": "RSI - Aurora MR",
         "description": "light Freight",
-        "image": "../images/vaisseaux/Aurora",
+        "image": "../images/vaisseaux/Aurora.jpg",
         "price": "30€"
     },
     {
         "name": "Consolidated Outland - Mustang Alpha",
         "description": "light Freight",
-        "image": "../images/vaisseaux/Mustang",
+        "image": "../images/vaisseaux/Mustang.jpg",
         "price": "35€"
     },
     {
         "name": "Aegis Dynamics - Avenger",
         "description": "light freight",
-        "image": "../images/vaisseaux/Avenger",
+        "image": "../images/vaisseaux/Avenger.jpg",
         "price": "72€"
     },
     {
         "name": "RSI - Constellation aquila",
         "description": "Exploration",
-        "image": "../images/vaisseaux/Constellation",
+        "image": "../images/vaisseaux/Constellation.jpg",
         "price": "340€"
     },
     {
         "name": "Anvil - Carrack",
         "description": "Exploration",
-        "image": "../images/vaisseaux/Carrack",
+        "image": "../images/vaisseaux/Carrack.jpg",
         "price": "550€"
+    },
+    {
+        "name": "Origin - 890 Jump",
+        "description": "Luxury",
+        "image": "../images/vaisseaux/890Jump.jpg",
+        "price": "1037€"
+    },
+    {
+        "name": "ARGO - MOLE",
+        "description": "Mining",
+        "image": "../images/vaisseaux/MOLE.jpg",
+        "price": "343€"
+    },
+    {
+        "name": "Aegis Dynamics - Gladius",
+        "description": "Fighter",
+        "image": "../images/vaisseaux/Gladius.jpg",
+        "price": "98€"
     },
 ]
