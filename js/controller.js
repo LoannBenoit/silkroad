dc688ec538ec29f0cbdfb6fe614e68817b922849
SullivanMICHEL <sullivan.michel@icloud.com>
2020-01-26 16:45:36 +0100

    Implémentation du panier

--- js/controller.js
+++ js/controller.js
@@ -7,6 +7,7 @@
  */
 
 const OC = Window.ORMClient;
+var tabPanier = new Array();
 
 //let product_with_id_1 = OC.getORM().loadCatalog("catalog_highTech").findById(1);
 
@@ -27,30 +28,32 @@ const OC = Window.ORMClient;
     
 // });
 
+//Chargement du premier catalogue au démarage
+$("#productCards").empty();
+loadCatalog("catalog_fruit");
 
 
 $( "#catalogFruits" ).click(function() {
-    $("#productCards").empty();
-    loadCatalog("catalog_fruit");
-  });
+$("#productCards").empty();
+loadCatalog("catalog_fruit");
+});
 
-  $( "#catalogSpace" ).click(function() {
-    $("#productCards").empty();
-    loadCatalog("catalog_space");
-  });
+$( "#catalogSpace" ).click(function() {
+$("#productCards").empty();
+loadCatalog("catalog_space");
+});
 
-  $( "#catalogHighTech" ).click(function() {
-    $("#productCards").empty();
-    loadCatalog("catalog_highTech");
-  });
-  
-  $( "#catalogTest" ).click(function() {
-    $("#productCards").empty();
-    loadCatalog("catalogTest");
-  });
+$( "#catalogHighTech" ).click(function() {
+$("#productCards").empty();
+loadCatalog("catalog_highTech");
+});
 
+$( "#catalogTest" ).click(function() {
+$("#productCards").empty();
+loadCatalog("catalogTest");
+});
 
-  function loadCatalog(catalogName){
+function loadCatalog(catalogName){
     let all_products = OC.getORM().loadCatalog(catalogName).findAll();
     var i = 0;
     all_products.forEach(element => {
@@ -62,10 +65,30 @@ $( "#catalogFruits" ).click(function() {
     $("<div></div>", {id: 'productPhoto_'+i, class: 'productPhoto'}).appendTo("#cardBackground_"+i);
     $("<img>", {src: element.image}).appendTo("#productPhoto_"+i);
     $("<div></div>", {id: 'productFooter_'+i, class: 'productFooter'}).appendTo("#cardBackground_"+i);
-    $("<p></p>", {id: 'productPricePara_'+i, class: 'productPricePara', text: element.price + ' €'}).appendTo("#productFooter_"+i);
-    $("<input>", {type: 'number', name: 'amount', value: '0', id: 'productAmount'}).appendTo("#productFooter_"+i);
-    $("div></div>", {class: 'icon', text: "Chariot"}).appendTo("#productFooter_"+i);
+    $("<p></p>", {id: 'productPricePara', class: 'productPricePara', text: element.price + ' €'}).appendTo("#productFooter_"+i);
+    $("<input>", {id: 'productAmount', type: 'number', name: 'amount', value: '0'}).appendTo("#productFooter_"+i);
+    $("<div></div>", {id: 'chariot_'+i, class: 'icon', text: "Chariot"}).appendTo("#productFooter_"+i);
 
     i++;
     });
-  }
+}
+
+$(".icon").click(function() {
+    var div = "#"+this.id;
+    var quantite = $(div).parents().children("#productAmount").val();
+    var prix = $(div).parents().children("#productPricePara").text();
+    var nom = $("#"+$(div).parents().attr("id")).parents().children(".productName").text();
+    quantite = parseInt(quantite);
+    prix = parseFloat(prix);
+
+    if(quantite > 0){
+        addProduct(nom,quantite,prix)
+    }
+    
+})
+
+function addProduct(nom, quantite, prix){
+    console.log(nom+"/"+quantite+"/"+prix);
+
+    $("<p></p>", {id: 'prod_panier', class: 'productList', text: quantite + " X " + nom +" ("+ prix +" €/pcs)"+ " - " + prix*quantite + " €"}).appendTo("#productList");
+}
